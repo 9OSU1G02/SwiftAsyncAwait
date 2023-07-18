bacb7ea961783f3a03711ae57356a09e6b1273e4
Nguyen Quoc Huy <huynq@apero.vn>
2023-07-18 17:52:34 +0700

    Blabber Chat app

---
+++ Blabber/Blabber/BlabberApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct BlabberApp: App {
+  var body: some Scene {
+    WindowGroup {
+      LoginView()
+    }
+  }
+}
---
+++ Blabber/Blabber/Utility/ChatLocationDelegate.swift
@@ -0,0 +1,6 @@
+
+import Foundation
+import CoreLocation
+
+class ChatLocationDelegate: NSObject, CLLocationManagerDelegate {
+}
